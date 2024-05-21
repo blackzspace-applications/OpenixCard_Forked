ee41e6d128cb9fd5c29814889e0f75bb5d8254b8
BlackzByt3 <blackbyte@blackzspace.de>
2024-05-21 06:16:03 +0200

    added

---
+++ lib/ColorCout/README.md
@@ -0,0 +1,51 @@
+<h1 align="center">Welcome to ColorCout 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.0.2-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/GloomyGhost-MosquitoSeal/ColorCout/blob/master/LICENSE" target="_blank">
+    <img alt="License: Do What The F*ck You Want To Public License" src="https://img.shields.io/badge/License-Do What The F*ck You Want To Public License-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/GLGH\_" target="_blank">
+    <img alt="Twitter: GLGH\_" src="https://img.shields.io/twitter/follow/GLGH_.svg?style=social" />
+  </a>
+</p>
+
+> Simple cross-platform(?) colored terminal text in C++
+
+### 🏠 [Homepage](https://github.com/GloomyGhost-MosquitoSeal/ColorCout)
+
+### ✨ Demo
+![Demo](https://user-images.githubusercontent.com/12003087/78259083-c6a6ff80-752e-11ea-8d16-0a45fcc811c9.png)
+
+## Usage
+
+```sh
+std::cout << cc::red << " red "
+          << cc::yellow << " yellow "
+          << cc::green << " green "
+          << cc::reset << " reset"
+          << std::endl;
+```
+
+## Author
+
+👤 **GloomyGhost-MosquitoSeal**
+
+* Website: https://gloomyghost.com/
+* Twitter: [@GLGH\_](https://twitter.com/GLGH\_)
+* Github: [@YuzukiTsuru](https://github.com/YuzukiTsuru)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/YuzukiTsuru/ColorCout/issues). 
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2020 [YuzukiTsuru](https://github.com/YuzukiTsuru).<br />
+This project is [Do What The F*ck You Want To Public License](https://github.com/YuzukiTsuru/ColorCout/blob/master/LICENSE) licensed.
+
+## Reference
+[https://github.com/yurablok/colored-cout/](https://github.com/yurablok/colored-cout/) for the nice idea.
---
+++ lib/ColorCout/includes/ColorCout.hpp
@@ -0,0 +1,103 @@
+/*
+                     ColorCout
+                     - ColorCout.hpp
+
+               Version 1, April 2020
+Copyright (C) 2020 GloomyGhost <GloomyGhost@foxmail.com>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+#pragma once
+
+#ifndef COLORBOX_COLORCOUT_H
+#define COLORBOX_COLORCOUT_H
+
+#include <iostream>
+#include <cstdint>
+
+#ifdef WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#if MSVC
+#define NOMINMAX
+#endif
+#include <Windows.h>
+#include <wincon.h>
+
+enum class cc : uint16_t {
+      grey       = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED
+    , blue       = FOREGROUND_BLUE | FOREGROUND_INTENSITY
+    , green      = FOREGROUND_GREEN | FOREGROUND_INTENSITY
+    , cyan       = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY
+    , red        = FOREGROUND_RED | FOREGROUND_INTENSITY
+    , magenta    = FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY
+    , yellow     = FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY
+    , white      = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY
+    , on_blue    = BACKGROUND_BLUE
+    , on_red     = BACKGROUND_RED
+    , on_magenta = BACKGROUND_BLUE | BACKGROUND_RED
+    , on_grey    = BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED
+    , on_green   = BACKGROUND_GREEN | BACKGROUND_INTENSITY
+    , on_cyan    = BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_INTENSITY
+    , on_yellow  = BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY
+    , on_white   = BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY
+    , reset
+    };
+#else
+enum class cc : uint8_t {
+    grey = 30, red = 31, green = 32, yellow = 33,
+    blue = 34, magenta = 35, cyan = 36, white = 37,
+    on_grey = 40, on_red = 41, on_green = 42, on_yellow = 43,
+    on_blue = 44, on_magenta = 45, on_cyan = 46, on_white = 47,
+    reset
+};
+#endif
+
+template<typename type>
+type &operator<<(type &ostream, const cc color) {
+#ifdef WIN32
+    static const uint16_t initial_attributes = [] {
+        CONSOLE_SCREEN_BUFFER_INFO buffer_info;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &buffer_info);
+        return buffer_info.wAttributes;
+    }();
+    static uint16_t background = initial_attributes & 0x00F0;
+    static uint16_t foreground = initial_attributes & 0x000F;
+#endif
+    if (color == cc::reset) {
+#ifdef WIN32
+        ostream.flush();
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), initial_attributes);
+#else
+        ostream << "\033[m";
+#endif
+    } else {
+#ifdef WIN32
+        uint16_t set = 0;
+        if (static_cast<uint16_t>(color) & 0x00F0) {
+            background = static_cast<uint16_t>(color);
+            set = background | foreground;
+        }
+        else if (static_cast<uint16_t>(color) & 0x000F) {
+            foreground = static_cast<uint16_t>(color);
+            set = background | foreground;
+        }
+        ostream.flush();
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), set);
+#else
+        ostream << "\033[" << static_cast<uint32_t>(color) << "m";
+#endif
+    }
+    return ostream;
+}
+
+#endif //COLORBOX_COLORCOUT_H
